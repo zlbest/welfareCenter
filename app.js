dc9b2a76953cb95aa28e9fcb59575963abd3db1c
zhanglin <zhanglin@zhanglindeMacBook-Air.local>
2019-07-26 11:24:31 +0800

    create project catalogue

---
+++ app.js
@@ -0,0 +1,56 @@
+//app.js
+App({
+  onLaunch: function () {
+    // 登录
+    wx.login({
+      success: res => {
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+      }
+    });
+    
+    // 获取用户信息
+    wx.getSetting({
+      success: res => {
+        if (res.authSetting['scope.userInfo']) {
+          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+          wx.getUserInfo({
+            success: res => {
+              // 可以将 res 发送给后台解码出 unionId
+              this.globalData.userInfo = res.userInfo
+
+              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+              // 所以此处加入 callback 以防止这种情况
+              if (this.userInfoReadyCallback) {
+                this.userInfoReadyCallback(res)
+              }
+            }
+          })
+        }
+      }
+    });
+  },
+  globalData: {
+    baseUrl: 'https://schep-cdn-stg.pingan.com.cn'
+  },
+
+  POST: function(url, params) {
+    let that = this;
+    return new Promise(function (resolve, reject) {
+      wx.request({
+        url: that.globalData.baseUrl + url,
+        method: 'post',
+        data: params,
+        header: { 
+          'Content-Type': 'application/json;charset=UTF-8',
+          'token': wx.getStorageSync('token')
+         },
+        success: function (res) {
+          resolve(res)
+        },
+        fail: function (e) {
+          reject(res)
+        }
+      });  
+    });
+  }
+})
