0a291b1f2b58664bfd33da1bbb069cfefd31c44c
zhanglin <15800678225@163.com>
2019-07-29 16:49:17 +0800

    收货地址

--- app.js
+++ app.js
@@ -2,8 +2,8 @@
 App({
   onLaunch: function () {
     //设置token信息
-    wx.setStorageSync('token', '')
-    
+    wx.setStorageSync('token', '9cd000e1-cca7-42ab-aa9a-ec56fe6f140b')
+    wx.setStorageSync('client', 'H5-HRX')
   },
   globalData: {
     baseUrl: 'https://schep-cdn-stg.pingan.com.cn'
@@ -18,7 +18,8 @@ App({
         data: params,
         header: { 
           'Content-Type': 'application/json;charset=UTF-8',
-          'token': wx.getStorageSync('token')
+          'token': wx.getStorageSync('token'),
+          'client': wx.getStorageSync('client')
          },
         success: function (res) {
           resolve(res)
