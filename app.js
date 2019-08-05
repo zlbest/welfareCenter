f8281badef46859713909d7771911327aad3e8b3
zhanglin <15800678225@163.com>
2019-08-05 17:47:54 +0800

    商品详情

--- app.js
+++ app.js
@@ -4,6 +4,15 @@ App({
     //设置token信息
     wx.setStorageSync('token', '78dc6887-75d3-488a-8033-6cfb6ff5ba5a')
     wx.setStorageSync('client', 'H5-HRX')
+    // wx.getSystemInfo({
+    //   success(res) {
+    //     if (/iOS/i.test(res.system)) {
+    //       wx.setStorageSync('client', 'IOS')
+    //     } else {
+    //       wx.setStorageSync('client', 'Android')
+    //     }
+    //   }
+    // })
   },
   globalData: {
     baseUrl: 'https://schep-cdn-stg.pingan.com.cn'
